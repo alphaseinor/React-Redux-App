63abfa7a28d91318b1768a199dd72d6acea9a7ca
Brian Hague <alphaseinor@gmail.com>
2020-01-23 12:32:53 -0600

    added some boilerplate, and dependencies

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isLoading: false,
+  activity: {},
+  error: ""
+}
+
+export const reducer = (state = initialState, action) => {
+  switch(action.type){
+    default:
+      return state
+  }
+}
