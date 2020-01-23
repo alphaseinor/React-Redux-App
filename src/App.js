f95e1d00240e5d1dcabca9f31196789cfa77a79b
Brian Hague <alphaseinor@gmail.com>
2020-01-23 15:35:21 -0600

    added axios and baseURL

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Activity from './components/Activity'
+import Rocket from './components/Rocket'
 
 function App() {
   return (
     <>
-      <Activity />
+      <Rocket />
     </>
   );
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,11 @@
+import {
+  FETCHING_ROCKET_START, 
+  FETCHING_ROCKET_ERROR,
+  FETCHING_ROCKET_SUCCESS
+} from '../actions'
+
 const initialState = {
+  baseURL: 'https://api.spacexdata.com/v3',
   isLoading: false,
   rocket: null,
   error: ""
@@ -6,7 +13,25 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
   switch(action.type){
+    case FETCHING_ROCKET_START:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case FETCHING_ROCKET_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        rocket: action.payload
+      }
+    case FETCHING_ROCKET_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
     default:
       return state
   }
 }
+
